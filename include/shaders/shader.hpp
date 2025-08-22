31fcb799bb79629c5a118f9e02a924efccc70581
Jeff Ahn <ahn.jeff@gmail.com>
2025-08-21 17:19:16 -0700

    add set_bool to Shader class to test the class is portable

--- include/shaders/shader.hpp
+++ include/shaders/shader.hpp
@@ -1,25 +1,26 @@
 #pragma once
 
+#include <iostream>
+#include <string>
 #include "glad.h"
 #include "glfw3.h"
-#include <string>
-#include <iostream>
-
 
 class Shader {
-    public:
-    unsigned int programID;
-    std::string vertexFile;
-    std::string fragmentFile;
+ public:
+  unsigned int programID;
+  std::string vertexFile;
+  std::string fragmentFile;
 
-    long fragmentModTimeOnLoad;
+  long fragmentModTimeOnLoad;
 
-    Shader();
-    void Unload();
-    void ReloadFromFile();
-    static Shader LoadShader(std::string fileVertexShader, std::string fileFragmentShader);
+  Shader();
+  void Unload();
+  void ReloadFromFile();
+  static Shader LoadShader(std::string fileVertexShader,
+                           std::string fileFragmentShader);
+  void set_bool(const std::string& name, bool value) const;
 
-    private:
-    static bool CompileShader(unsigned int shaderId, char(&infoLog)[512]);
-    static bool LinkProgram(unsigned int programID, char(&infoLog)[512]);
+ private:
+  static bool CompileShader(unsigned int shaderId, char (&infoLog)[512]);
+  static bool LinkProgram(unsigned int programID, char (&infoLog)[512]);
 };
