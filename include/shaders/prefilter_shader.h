1dc596065c94b637aaecd12235dc328c38724f93
Jeff Ahn <ahn.jeff@gmail.com>
2025-09-04 20:24:41 -0700

    implement generatetextures

--- include/shaders/prefilter_shader.h
+++ include/shaders/prefilter_shader.h
@@ -1,12 +1,21 @@
 #ifndef PREFILTER_SHADER_H_
 #define PREFILTER_SHADER_H_
 
+#define GLAD_GL_IMPLEMENTATION
+#include "glad.h"
+
 #include "shaders/shader.h"
 
 class PrefilterShader : public Shader {
  public:
   PrefilterShader() = default;
   PrefilterShader(std::string fileVertexShader, std::string fileFragmentShader);
+
+  void GenerateTextures() override;
+  void BindAllTextures() override;
+
+ private:
+  unsigned int prefilter_map_texture_;
 };
 
 #endif
--- src/shaders/prefilter_shader.cpp
+++ src/shaders/prefilter_shader.cpp
@@ -3,3 +3,22 @@
 PrefilterShader::PrefilterShader(std::string fileVertexShader,
                                  std::string fileFragmentShader)
     : Shader(fileVertexShader, fileFragmentShader) {}
+
+void PrefilterShader::GenerateTextures() {
+  glGenTextures(1, &prefilter_map_texture_);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, prefilter_map_texture_);
+  for (unsigned int i = 0; i < 6; ++i) {
+    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 128, 128, 0,
+                 GL_RGB, GL_FLOAT, nullptr);
+  }
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+  // Be sure to set minification filter to mip_linear
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+}
+
+void PrefilterShader::BindAllTextures() {}
